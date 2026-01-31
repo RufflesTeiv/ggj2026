1804d8c8cfa6992d06a149daec8aa422b302bb1a
Rafa Teivfik <teivfik@gmail.com>
2026-01-31 11:11:09 -0300

    Adicionada estrutura de pastas

---
+++ script_templates/Object/base_model.gd
@@ -0,0 +1,23 @@
+class_name BaseModel
+
+#region Enums
+#endregion
+
+#region Variables
+#endregion
+
+#region Computed properties
+#endregion
+
+func _init():
+	pass
+
+#region Public functions
+#endregion
+
+#region Overridable functions
+#endregion
+
+#region Subclasses
+#endregion
+
