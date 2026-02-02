753026f63fbf0aa48ced1684ceb260cc95045153
Rafa Teivfik <teivfik@gmail.com>
2026-02-02 14:14:11 -0300

    Progresso no codigo multiplayer

--- managers/local_multiplayer_manager.gd
+++ managers/local_multiplayer_manager.gd
@@ -16,19 +16,14 @@ extends Node
 #endregion
 
 #region Default functions
-# func _init(): pass
-	
-# func _enter_tree(): pass
-	
-# func _ready(): pass
-	
-# func _process(_delta): pass
-	
-# func _physics_process(_delta): pass
-	
-# func _input(_event: InputEvent): pass
-
-# func _exit_tree(): pass
+#func _init(): pass
+#func _enter_tree(): pass
+func _ready():
+	_setup_multiplayer_inputs()
+#func _process(_delta): pass
+#func _physics_process(_delta): pass
+#func _input(_event: InputEvent): pass
+#func _exit_tree(): pass
 #endregion
 
 #region Public functions
@@ -38,6 +33,18 @@ extends Node
 #endregion
 
 #region Private functions
+func _setup_multiplayer_inputs():
+	for action in InputMap.get_actions():
+		if action.contains("ui_"): continue
+		_setup_multi_action(action,4)
+		
+func _setup_multi_action(action:StringName, player_count:int):
+	var events := InputMap.action_get_events(action)
+	for event in events:
+		if !(event is InputEventJoypadButton) and !(event is InputEventJoypadMotion):
+			continue
+		
+	
 #endregion
 
 #region Subclasses
